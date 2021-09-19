3d88b0ed6b555d0427a38e3b3f766e5a6f55bd49
Lindsey <lindsey_sather1@baylor.edu>
2021-09-19 01:03:53 -0500

    committttt

--- CarProgram.py
+++ CarProgram.py
@@ -2,15 +2,17 @@ import CarClass as c
 
 def main():
 
-    year_model = int(input("Enter your car's year model: "))
+    year_model = input("Enter your car's year model: ")
     make = input("Enter your car's make: ")
 
     my_car = c.Car(year_model, make)
 
+    print("\nYour", year_model, make, "is accelerating:")
     for count in range(5):
         my_car.accelerate()
         print("Current speed:", my_car.get_speed(), "mph")
 
+    print("\nYour", year_model, make, "is braking:")
     for count in range(5):
         my_car.brake()
         print("Current speed:", my_car.get_speed(), "mph")
